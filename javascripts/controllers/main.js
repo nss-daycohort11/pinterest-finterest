422b516299770cb331eaa652f56c546543f8190e
Andri Alexandrou <andri.j.alexandrou@gmail.com>
2015-12-07 15:07:36 -0600

    inserted some stolen code to achieve pinterest layering of images

--- javascripts/controllers/main.js
+++ javascripts/controllers/main.js
@@ -1,8 +1,6 @@
 app.controller("MainPinCtrl", ["$q", "$http", "$scope",
   function($q, $http, $scope) {
     console.log("thispagehasloaded");
-    var poop = flickr.photos.search();
-      console.log("this stuff here", poop);
 
 
   }]);
